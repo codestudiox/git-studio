4f435a236b3efe2e7572c2f87e89df1827a0b927
codestudiox <54465859+codestudiox@users.noreply.github.com>
2022-12-15 01:55:15 +0900

    last cmt

--- src/git-studio/index.js
+++ src/git-studio/index.js
@@ -5,7 +5,11 @@ export default function GitStudio() {
     <div>
       <h1>Git Studio</h1>
       <h2>Steps</h2>
-      
+      <p>
+      git reset --hard (돌아갈 커밋 해시)
+      </p>
+
+      git revert (되돌릴 커밋 해시)
     </div>
   );
 }
